7ebc5f7deb0b6f8976228dd27257d55ebebee4ca
SmartBro <smartbrostudio@gmail.com>
2016-08-06 16:55:32 +0300

    add HtmlPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,24 @@
 var join = require('path').join;
 var webpack = require('webpack');
+var HtmlPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/js/app.js',
-  output: { path: join(__dirname, 'build'), filename: 'bundle.js' },
-  module: {
-    loaders: [
-      {
-        test: /.js?$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-        query: {
-          presets: ['es2015', 'react']
-        }
-      }
+    entry: './src/js/app.js',
+    output: {path: join(__dirname, 'build'), filename: 'bundle.js'},
+    module: {
+        loaders: [
+            {
+                test: /.js?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    plugins: [
+        new HtmlPlugin({
+            filename: 'index.html',
+            template: 'src/index.html',
+            inject: true
+        })
     ]
-  },
 };
